0c39c2456d1244b67670dc37538f65c3b0c8dba8
RJAL <a027972@ismai.pt>
2017-01-05 04:04:58 +0000

    App update.

---
+++ app/src/main/java/com/example/ruilopes/readthemanga/AnimeFragment.java
@@ -0,0 +1,26 @@
+package com.example.ruilopes.readthemanga;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AnimeFragment extends Fragment {
+
+
+    public AnimeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_anime, container, false);
+    }
+
+}
